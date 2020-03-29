09a47c04a0abe3bb9980cc2015a4bea0a295dde1
Evans-Munatsa <evansmunatsa7@gmail.com>
2020-03-29 12:23:03 +0200

    authentication front-end

---
+++ src/core/Menu.js
@@ -0,0 +1,52 @@
+import React, {Fragment} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+import {signout, isAuthenticated} from '../auth/index';
+
+const isActive = (history, path) => {
+    if(history.location.pathname === path) {
+        return {color: '#ff9900'};
+    } else {
+        return {color: '#000000'};
+    }
+}
+
+const Menu = ({history}) => (
+    <div>
+        <ul className="nav justify-content-end">
+            <li className="nav-item">
+                <Link className="nav-link" style={isActive(history, "/")} to="/">
+                    Home
+                </Link>
+            </li>
+
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, "/signin")} to="/signin">
+                            Signin
+                        </Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link className="nav-link" style={isActive(history, "/signup")} to="/signup">
+                            SignUp
+                        </Link>
+                    </li>
+                </Fragment>
+            )}
+
+            {isAuthenticated() && (
+                <li className="nav-item">
+                    <span className="nav-link" style={{cursor: 'pointer', color: "#000000"}} onClick = {() => signout(() => {
+                        history.push('/')
+                        })
+                    } >
+                    Signout
+                    </span>
+                </li>  
+            )}
+        </ul>
+    </div>
+)
+
+export default withRouter(Menu);
